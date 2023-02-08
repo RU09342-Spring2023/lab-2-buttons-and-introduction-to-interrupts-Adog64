d7785bc8927421fcf340368a089ad2967eaa4dee
Adog64 <amsharpe102@pm.me>
2023-02-08 12:16:32 -0500

    Defined LED behavior for each state

--- 4 Designing an Intruder Security System/main.c
+++ 4 Designing an Intruder Security System/main.c
@@ -13,18 +13,24 @@
 #define WARNING 0x01
 #define ALERT 0x02
 
+#define HALF_SECOND 500000
 #define ONE_SECOND 1000000
-#define ONE_AND_A_HALF_SECONDS 1500000
+#define ONE_AND_A_HALF_SECONDS HALF_SECOND + ONE_SECOND
+#define TEN_SECONDS 10*ONE_SECOND
 
 char state = ARMED;
 
 int main(void)
 {
+    char warningTime;
     killWatchdogTimer();
     unlockGPIO();
 
     setPinMode(4,1, INPUT);
+    setPinMode(6,6, OUTPUT);
     setPinMode(1,0, OUTPUT);
+
+    setPinState(6,6, 0);
     setPinState(1,0, 0);
 
     while (true)
@@ -32,14 +38,23 @@ int main(void)
         switch (state)
         {
             case ARMED:
-                togglePinState(1,0);
+                setPinState(1,0, 0);
+                togglePinState(6,6);
                 __delay_cycles(ONE_AND_A_HALF_SECONDS);
                 break;
             case WARNING:
+                if (warningTime <= 0)
+                    state = ALERT; 
+                setPinState(6,6, 0);
+                togglePinState(1,0);
+                __delay_cycles(HALF_SECOND);
+                warningTime -= HALF_SECOND;
                 break;
+            case ALERT:
+                setPinState(1,0, 1);
+                setPinState(6,6, 0);
         }
     }
 }
-
 
 
