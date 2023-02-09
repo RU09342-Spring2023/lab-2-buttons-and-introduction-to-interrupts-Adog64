d861a9595ba86ea5dabb8e8809c29ad91da8f284
Adog64 <amsharpe102@pm.me>
2023-02-09 17:54:42 -0500

    Armed state running with updated pin functions

--- 4 Designing an Intruder Security System/embedded_utils.h
+++ 4 Designing an Intruder Security System/embedded_utils.h
@@ -50,60 +50,24 @@ void setPinState(char port , char pin, bool value)
 {
     char bit = BIT0 << pin;
     port -= 1;
-    int* out = 0x0202 + ((port >> 1) << 5) + (port & 1);
+    char* out = 0x0202 + ((port >> 1) << 5) + (port & 1);
     *out = (value) ? *out | bit : *out & ~bit;
 }
 
 void togglePinState(char port, char pin)
 {
-    unsigned int bit = BIT0 << pin;
-    switch (port)
-    {
-        case 1:
-            P1OUT ^= bit;
-            break;
-        case 2:
-            P2OUT ^= bit;
-            break;
-        case 3:
-            P3OUT ^= bit;
-            break;
-        case 4:
-            P4OUT ^= bit;
-            break;
-        case 5:
-            P5OUT ^= bit;
-            break;
-        case 6:
-            P6OUT ^= bit;
-            break;
-    }
+    char bit = BIT0 << pin;
+    port -= 1;
+    char* out = 0x0202 + ((port >> 1) << 5) + (port & 1);
+    *out ^= bit;
 }
 
 void enablePinInterrupt(char port, char pin)
 {
     char bit = BIT0 << pin;
-    switch (port)
-    {
-    case 1:
-        P1IE |= bit;
-        break;
-    case 2:
-        P2IE |= bit;
-        break;
-    case 3:
-        P3IE |= bit;
-        break;
-    case 4:
-        P4IE |= bit;
-        break;
-    case 5:
-        P5IE |= bit;
-        break;
-    case 6:
-        P6IE |= bit;
-        break;
-    }
+    port -= 1;
+    char* ie = 0x021A + ((port >> 1) << 5) + (port & 1);
+    *ie |= bit;
 }
 
 
